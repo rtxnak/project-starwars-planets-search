576bf7ba05b6b3b3cbf6d45fae3fa84be98f36b3
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-01-25 16:57:38 -0300

    feat[req01] - adicionou estrutura basica da tabela recebendo informacoes da API

---
+++ src/components/Table.jsx
@@ -0,0 +1,56 @@
+import React, { useContext } from 'react';
+import SWContext from '../context/SwContext';
+
+function Table() {
+  const { planets } = useContext(SWContext);
+
+  if (!planets || planets.length <= 0) {
+    return <h2>Loading...</h2>;
+  }
+  // console.log(planets.results);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          planets.results.map((planet, i) => (
+            <tr key={ i }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films.map((film) => film) }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
